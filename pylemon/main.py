22e3a00e44f7879fc0bdb4a42b3937d2482ca07a
Timo Furrer <tuxtimo@gmail.com>
2014-04-27 20:05:36 +0200

    abort on config parse failure

--- pylemon/main.py
+++ pylemon/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from sys import exit
+
 from logger import Logger
 from config import Config
 from monitor import Monitor
@@ -12,7 +14,9 @@ def main():
     logger.info("started pylemon daemon")
 
     config = Config()
-    config.parse("/etc/pylemon.conf")
+    if not config.parse("/etc/pylemon.conf"):
+        logger.error("abort pylemon because of failure during config parse")
+        exit(1)
 
     monitor = Monitor(config)
     monitor.watch_config()
